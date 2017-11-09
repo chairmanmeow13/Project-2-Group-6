9ab9c71d1ed0d50fafda9faf8e2288ee6159c1c3
Loren <lorenrose@icloud.com>
2017-11-08 18:31:14 -0600

    new files

---
+++ sentence-quiz/app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+var scoreList = require('../data/scores.js');
+
+module.exports = function(app){
+  //a GET route that displays prev high scores?
+
+  app.post('/api/scores', function(req,res){
+    var newScore = //logic;
+    var scoresArray = [];
+
+    //logic to score module
+
+      //push results into scoresArray
+      scoresArray.push(newScore);
+    }
+
+};
---
+++ sentence-quiz/app/routing/htmlRoutes.js
@@ -0,0 +1,7 @@
+var path = require("path");
+
+//get route to home page
+
+//get route to user profile? high score page?
+
+//restart app option?
